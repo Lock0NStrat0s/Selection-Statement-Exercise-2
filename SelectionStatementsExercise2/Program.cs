79ab0d33782544800cf60243ee4479dd9d5cb555
Mohammad Jabir <mohammadj94@gmail.com>
2024-06-05 09:50:02 -0600

    exercise complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter your favorite school subject: ");
+            string userInput = Console.ReadLine().ToLower();
+
+            switch (userInput)
+            {
+                case "math":
+                    Console.WriteLine("Math is fun!");
+                    break;
+                case "science":
+                    Console.WriteLine("Are you Bill Nye the Science Guy??");
+                    break;
+                case "english":
+                    Console.WriteLine("Get those creative juices flowing!");
+                    break;
+                case "gym":
+                    Console.WriteLine("We got an athlete in the making!");
+                    break;
+                case "history":
+                    Console.WriteLine("Hindsight 2020!");
+                    break;
+                default:
+                    Console.WriteLine("Le wrong selection...");
+                    break;
+            }
         }
     }
 }
